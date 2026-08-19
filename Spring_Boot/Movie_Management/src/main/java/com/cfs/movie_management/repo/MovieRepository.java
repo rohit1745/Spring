6655bf9f88b45492e2a3a29c16f09354c85ff9e2
Rohit <rohitbadgujar535@gmail.com>
2026-08-19 22:29:31 +0530

    Movie Management

--- Spring_Boot/Movie_Management/src/main/java/com/cfs/movie_management/repo/MovieRepository.java
+++ Spring_Boot/Movie_Management/src/main/java/com/cfs/movie_management/repo/MovieRepository.java
@@ -9,6 +9,14 @@ public class MovieRepository {
 
     private final Map<Long , movie> movieDB=new HashMap<>();
 
+    public void loadDummyData()
+    {
+        movieDB.put(1L,new movie(1L,"Salar","Action",9.8,2022,"Hindi"));
+        movieDB.put(2L,new movie(2L,"KGF","Action-romance",9.5,2022,"Hindi"));
+        movieDB.put(3L,new movie(3L,"King of Kotha","Action",9.8,2023,"Hindi"));
+        movieDB.put(4L,new movie(4L,"Spider-man","Action-drama",9.8,2026,"English"));
+
+    }
 
     public List<movie> getAllMovies()
     {
