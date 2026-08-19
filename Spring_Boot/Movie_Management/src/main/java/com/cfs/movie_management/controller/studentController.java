ad42c254caaeb1f05e43171165c0a68a5721fe6c
Rohit <rohitbadgujar535@gmail.com>
2026-08-19 22:46:27 +0530

    Movie Management

--- Spring_Boot/Movie_Management/src/main/java/com/cfs/movie_management/controller/studentController.java
+++ Spring_Boot/Movie_Management/src/main/java/com/cfs/movie_management/controller/studentController.java
@@ -1,4 +1,48 @@
 package com.cfs.movie_management.controller;
 
+
+import com.cfs.movie_management.model.movie;
+import com.cfs.movie_management.service.movieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movie")
 public class studentController {
+
+    @Autowired
+    private movieService movieService;
+
+
+
+    @GetMapping("/all")
+    public List<movie> getAllMovie()
+    {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public movie getMovieByID(@PathVariable Long id)
+    {
+        return movieService.getMovieByID(id);
+
+    }
+
+
+    @GetMapping("/search")
+    public List<movie> searchByMovieName(@RequestParam String name)
+    {
+        return movieService.searchByName(name);
+    }
+
+    @PostMapping
+    public List<movie> save()
+    {
+        return movieService.getAllMovies();
+    }
+
+
 }
